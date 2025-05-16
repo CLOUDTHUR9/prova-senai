b72c14132fb6b291f341bebdf7a443b398cff3f8
Arthur Martins <arthurhpm0519@gmail.com>
2025-05-16 11:41:39 -0300

    first commit

--- src/routes.ts
+++ src/routes.ts
@@ -18,7 +18,7 @@ router.get("/listar/alunos", AlunoController.todos);
 // Rota para cadastrar um novo médico
 router.post("/cadastro/aluno", AlunoController.novo);
 // Rota para deletar um novo médico
-router.put("/remover/aluno/:idAluno", AlunoController.remover);
+router.delete("/remover/aluno/:idAluno", AlunoController.remover);
 // Rota para atualizar um novo médico
 router.put("/atualizar/aluno/:idAluno", AlunoController.atualizar);
 
@@ -31,7 +31,7 @@ router.get("/listar/cursos", CursoController.todos);
 // Rota para cadastrar um novo curso
 router.post("/cadastro/curso", CursoController.novo);
 // Rota para deletar um novo curso
-router.put("/remover/curso/:idCurso", CursoController.remover);
+router.delete("/remover/curso/:idCurso", CursoController.remover);
 // Rota para atualizar um novo curso
 router.put("/atualizar/curso/:idCurso", CursoController.atualizar);
 
@@ -44,7 +44,7 @@ router.get("/listar/matriculas", MatriculaController.todos);
 // Rota para cadastrar uma nova consulta
 router.post("/cadastro/matricula", MatriculaController.novo);
 // Rota para deltar uma nova consulta
-router.put("/remover/matricula/:idMatricula", MatriculaController.remover);
+router.delete("/remover/matricula/:idMatricula", MatriculaController.remover);
 // Rota para atualizar uma nova consulta
 router.put("/atualizar/matricula/:idMatricula", MatriculaController.atualizar);
 
